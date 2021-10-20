8434120aa09c4ca6aa9f5bcb39186a7950d761b0
Hassan Baig <hassan@mzwebstudio.com>
2021-10-20 07:31:33 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import NavComponent from './NavComponent';
+import About from './About';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Playbook from './Playbook';
 
 function App() {
   return (
+    <Router>
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavComponent/>
+      <Switch>
+      <Route path="/" exact component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/playbook" component={Playbook}/>
+      </Switch>
     </div>
+    </Router>
   );
 }
-
+const Home = () =>(
+<div>
+  <h1>
+    HOME PAGE
+  </h1>
+</div>
+);
 export default App;
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropType from 'prop-types'
+import styles from './modal.module.css'
+
+function Modal({show , onClose,childern ,backdropStyles , modalStyles}) {
+    return (
+        <>
+        <div className={styles.backdrop} style={{...backdropStyles}}>
+            <div className={styles.modal} style={{...modalStyles}}>
+                <div className={styles.modalContent}>
+                    {childern}
+                </div>
+            </div>
+        </div>
+        </>   
+    )
+}
+
+Modal.prototypes={
+    show:PropType.bool.isRequired,
+    onClose:PropType.func.isRequired,
+    childern:PropType.node.isRequired,
+    backdropStyles:PropType.object,
+    modalStyles:PropType.object
+}
+
+export default Modal
