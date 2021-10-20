61287beb2fb4560abecab60284d8f792e499ebd1
Abdussami Tayyab <sami@tintash.com>
2021-10-20 12:53:43 +0500

    what: fixed code so that modal can show content
    what: linted a few files along the way
    
    impact: high

--- src/NavComponent.js
+++ src/NavComponent.js
@@ -1,25 +1,25 @@
 import './App.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 function NavComponent() {
 
-      const navLinkStyle={
-        color : 'white'
-      };
+  const navLinkStyle = {
+    color: 'white'
+  };
 
-  return ( 
-      <nav>
-        <Link style = {navLinkStyle} to="/">
-           <h3>Project Manager PlayBook</h3>
-         </Link>
-        <ul className="nav-links">
-          <Link style={navLinkStyle} to="/playbook">
-            <li>PLAYBOOK</li>
-          </Link>
-          <Link style ={navLinkStyle} to="/about">
-             <li>ABOUT</li>
-          </Link>
-           </ul>
-      </nav>
+  return (
+    <nav>
+      <Link style={navLinkStyle} to="/">
+        <h3>Project Manager PlayBook</h3>
+      </Link>
+      <ul className="nav-links">
+        <Link style={navLinkStyle} to="/playbook">
+          <li>PLAYBOOK</li>
+        </Link>
+        <Link style={navLinkStyle} to="/about">
+          <li>ABOUT</li>
+        </Link>
+      </ul>
+    </nav>
   );
 }
 
